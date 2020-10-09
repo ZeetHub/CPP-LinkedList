3cc7e5678daa3f9baad6e92399dba4c751d3256e
Zeet <zeet09@gmail.com>
2020-10-09 15:08:55 +0300

    commit

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -26,18 +26,48 @@ int isEmpty(LinkedList<T> list)
 }
 
 template <class T>
+int insertNode(LinkedList<T> list, int p)//This function is used only when dealing with a sorted list.
+{
+    //return a pointer to the preceding element after which node p is to be inserted. 
+    int found=0;
+    int prev=NIL;
+    while(list.head != NIL && !found)
+    {
+        if(list.array->base[list.head].data < list.array->base[p].data)
+        {
+            prev = list.head;
+            list.head = list.array->base[list.head].next;
+        }
+        else
+        {
+            found = 1;
+        }
+    }
+    return prev;
+}
+
+
+
+template <class T>
 void insertNode(LinkedList<T> &list, int p, int prev)
 {
     //insert a new node pointed by p after a node pointed by prev
-    if(list.head ==NIL)
+    if(list.head == NIL)
     {
         list.array->base[p].next = NIL;//vs list.array.base[p]->next = NIL;
         list.head=list.tail=p;
     }
-    else if(prev==NIL)
+    else if(prev == NIL)
     {
-        list.array->base[p].next = list.head;
+        list.array->base[p].next = list.head;//vs list.array.base[p]->next = list.head;
         list.head = p;
     }
+    else
+    {
+        list.array->base[p].next = list.array->base[prev].next;
+        list.array->base[prev].next = p;
+        if(prev == list.tail)
+            list.tail = p;
+    }
+    
 }
-
